c1f62d2309e4dec8deb6f2ce3f4a3e25317932e3
Arjun Earthperson <a.earthperson@openpra.org>
2024-11-18 20:15:16 -0500

    - migrate to pyproject.toml - add CI pipeline - migrate to MIT license - add linting support using ruff - editor configs

--- .space.kts
+++ .space.kts
@@ -1,4 +1,4 @@
-job("OrderDD CI") {
+job("orderdd") {
 
     requirements {
         workerTags("swarm-worker")
@@ -8,74 +8,84 @@ job("OrderDD CI") {
     val image = "orderdd"
     val remote = "$registry$image"
 
-  host("Image Tags") {
-    // use kotlinScript blocks for usage of parameters
-    kotlinScript("Generate slugs") { api ->
+    host("Image Tags") {
+        // use kotlinScript blocks for usage of parameters
+        kotlinScript("Generate slugs") { api ->
 
-      api.parameters["commitRef"] = api.gitRevision()
-      api.parameters["gitBranch"] = api.gitBranch()
+            api.parameters["commitRef"] = api.gitRevision()
+            api.parameters["gitBranch"] = api.gitBranch()
 
-      val branchName = api.gitBranch()
-        .removePrefix("refs/heads/")
-        .replace(Regex("[^A-Za-z0-9-]"), "-") // Replace all non-alphanumeric characters except hyphens with hyphens
-        .replace(Regex("-+"), "-") // Replace multiple consecutive hyphens with a single hyphen
-        .lowercase() // Convert to lower case for consistency
+            val branchName = api.gitBranch()
+                .removePrefix("refs/heads/")
+                .replace(
+                    Regex("[^A-Za-z0-9-]"),
+                    "-"
+                ) // Replace all non-alphanumeric characters except hyphens with hyphens
+                .replace(Regex("-+"), "-") // Replace multiple consecutive hyphens with a single hyphen
+                .lowercase() // Convert to lower case for consistency
 
-      val maxSlugLength = if (branchName.length > 48) 48 else branchName.length
-      var branchSlug = branchName.subSequence(0, maxSlugLength).toString()
-      api.parameters["branchSlug"] = branchSlug
+            val maxSlugLength = if (branchName.length > 48) 48 else branchName.length
+            var branchSlug = branchName.subSequence(0, maxSlugLength).toString()
+            api.parameters["branchSlug"] = branchSlug
 
-      api.parameters["isMainBranch"] = (api.gitBranch() == "refs/heads/main").toString()
+            api.parameters["isMainBranch"] = (api.gitBranch() == "refs/heads/main").toString()
 
+        }
     }
-  }
 
-    host("Build & Test") {
-
-      shellScript("build"){
+    host("build-image") {
+      shellScript {
         interpreter = "/bin/bash"
         content = """
-                          docker build --tag="$remote:{{ branchSlug }}" .
-                          """
+                        docker pull $remote:{{ branchSlug }} || true
+                        docker build --tag="$remote:{{ branchSlug }}" --tag="$remote:ci-{{ run:number }}-{{ branchSlug }}" .
+                        docker push "$remote:ci-{{ run:number }}-{{ branchSlug }}"
+                        """
       }
+    }
 
-      shellScript("tests"){
-        interpreter = "/bin/bash"
-        content = """
-                          docker run --rm "$remote:{{ branchSlug }}" pytest
+    parallel {
+
+        host("Tests") {
+            shellScript("pytest") {
+                interpreter = "/bin/bash"
+                content = """
+                          docker run --rm "$remote:ci-{{ run:number }}-{{ branchSlug }}" pytest
                           """
-      }
+            }
+        }
 
-      shellScript("coverage"){
-        interpreter = "/bin/bash"
-        content = """
-                          docker run --rm "$remote:{{ branchSlug }}" pytest --cov
+        host("Coverage") {
+            shellScript("pytest --cov") {
+                interpreter = "/bin/bash"
+                content = """
+                          docker run --rm "$remote:ci-{{ run:number }}-{{ branchSlug }}" pytest --cov
                           """
-      }
+            }
+        }
 
-      shellScript("lint"){
-        interpreter = "/bin/bash"
-        content = """
-                          docker run --rm "$remote:{{ branchSlug }}" pylint /app/orderdd || exit 0
+        host("Lint & Format") {
+            shellScript("ruff check") {
+                interpreter = "/bin/bash"
+                content = """
+                          docker run --rm "$remote:ci-{{ run:number }}-{{ branchSlug }}" ruff check
                           """
-      }
-
+            }
+        }
     }
 
     host("Publish") {
 
-      runIf("{{ isMainBranch }}")
+        runIf("{{ isMainBranch }}")
 
-      env["USER"] = "{{ project:PYPI_USER_TOKEN }}"
-      env["PASSWORD"] = "{{ project:PYPI_PASSWORD_TOKEN }}"
+        env["TWINE_USERNAME"] = "{{ project:PYPI_USER_TOKEN }}"
+        env["TWINE_PASSWORD"] = "{{ project:PYPI_PASSWORD_TOKEN }}"
 
-      shellScript("build & push"){
-        interpreter = "/bin/bash"
-        content = """
-                          docker build --tag="$remote:{{ branchSlug }}" .
-                          docker run --rm "$remote:{{ branchSlug }}" /bin/bash -c "python setup.py sdist bdist_wheel && twine upload dist/* -u ${'$'}USER -p ${'$'}PASSWORD"
-                          docker rmi "$remote:{{ branchSlug }}"
-                          """
-      }
+        shellScript("build & package") {
+            interpreter = "/bin/bash"
+            content = """
+                      docker run --rm "$remote:ci-{{ run:number }}-{{ branchSlug }}" /bin/bash -c "python -m build && twine upload dist/*"
+                      """
+        }
     }
 }
